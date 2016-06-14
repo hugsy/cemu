1945054d949f7f44a765da401562e52c87cce944
hugsy <hugsy+github@blah.cat>
2016-06-15 08:49:15 +1000

    Issue #7: added cmake and pip3 check in requirements.sh file

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e 
+set -e
+
+CMAKE="$(which cmake)"
+if [ $? -ne 0 ]; then
+    echo "[-] You need 'cmake' to install 'cemu' requirements"
+    exit 1
+fi
+
+PIP3="$(which pip3)"
+if [ $? -ne 0 ]; then
+    echo "[-] You need 'pip' to install 'cemu' requirements"
+    exit 1
+fi
 
 pushd .
 
@@ -10,7 +22,7 @@ git clone https://github.com/keystone-engine/keystone.git
 mkdir -p keystone/build && cd keystone/build
 cmake .. && make -j8
 sudo make install
-cd ../bindings/python 
+cd ../bindings/python
 sudo make install3
 
 # capstone
@@ -19,7 +31,7 @@ git clone https://github.com/aquynh/capstone.git
 mkdir -p capstone/build && cd capstone/build
 cmake .. && make -j8
 sudo make install
-cd ../bindings/python 
+cd ../bindings/python
 sudo make install3
 
 # unicorn
@@ -28,10 +40,9 @@ git clone https://github.com/unicorn-engine/unicorn.git
 cd unicorn
 ./make.sh -j8
 sudo ./make.sh install
-cd ./bindings/python 
+cd ./bindings/python
 sudo make install3
 
 popd
 
 sudo pip3 install -r ./requirements.txt
-
