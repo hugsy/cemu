a46e2ca39c0015438e75460e1429d0526e7f8e13
Emin Ghuliev <drmint80@gmail.com>
2016-06-27 21:48:21 +0500

    Update reil.py

--- cemu/reil.py
+++ cemu/reil.py
@@ -2,12 +2,15 @@ import os
 import sys
 
 #Python 2 depended modules
+global reiluse
 try:
     __import__('imp').find_module('pyopenreil')
     from pyopenreil.REIL import *
     from pyopenreil.utils import asm
+    reiluse = True
 except ImportError:
-    pass
+	reiluse = False
+	pass
 
 
 class Reil:
@@ -25,11 +28,14 @@ class Reil:
 		self.widget.commandWidget.stopButton.setDisabled(False)
 		code = self.widget.codeWidget.getCleanCodeAsByte(as_string=True, parse_string=True)
 		code = tuple(synt for synt in code.split('\n') if synt)
-		try:
-			viasyntax = asm.Reader(ARCH_X86,(code),addr = 0)
-			store = CodeStorageTranslator(viasyntax)
-			irl = store.get_func(0)
-			for func in irl.bb_list: self.symprint(str(func).replace(" "*4," "))
-		except (ReadError,OSError):
-			self.symprint("An error occured when converting instructions into the symbolic form")
-		return
+		if reiluse:
+			try:
+				viasyntax = asm.Reader(ARCH_X86,(code),addr = 0)
+				store = CodeStorageTranslator(viasyntax)
+				irl = store.get_func(0)
+				for func in irl.bb_list: self.symprint(str(func).replace(" "*4," "))
+			except (ReadError,OSError):
+				self.symprint("An error occured when converting instructions into the symbolic form")
+			return
+		else:
+			self.symprint("pyopenREIL does not exists")
