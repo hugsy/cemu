7f8cb22e012e30d5e2b3a6ff164c94769cfe34ff
hugsy <hugsy+github@blah.cat>
2016-07-10 11:53:05 +1000

    new requirement installer for linux/osx

--- requirements.sh
+++ requirements.sh
@@ -1,40 +1,86 @@
 #!/bin/bash
+#
+# Basic requirement installer for cemu
+#
 
 
-CMAKE="$(which cmake)"
-if [ $? -ne 0 ]; then
-    echo "[-] You need 'cmake' to install 'cemu' requirements"
+if [ "$(uname)" == "Darwin" ]; then
+    echo "[+] Install for OSX"
+    PKG="$(which brew >/dev/null)"
+elif [ "$(uname)" == "Linux" ]; then
+    echo "[+] Install for Linux"
+    PKG="$(which dnf >/dev/null)"
+    if [ $? -ne 0 ]; then
+        PKG="$(which yum >/dev/null)"
+        if [ $? -ne 0 ]; then
+            PKG="$(which apt >/dev/null)"
+            if [ $? -ne 0 ]; then
+                echo "[-] No valid package manager found"
+                exit 1
+            fi
+        fi
+    fi
+else
+    echo "[-] Unsupported OS (this script supports only Linux or OSX)"
     exit 1
 fi
 
+echo "[+] Using package manager '${PKG}'"
 
-PYTHON3="$(which python3)"
-if [ $? -ne 0 ]; then
-    echo "[-] This script automates the installation for Python3 which was not found on your system."
-    exit 1
+if [ $1 == "--python2" ]; then
+    PYTHON=python2
+    PIP=pip
+    echo "[+] Install for python2"
+else
+    PYTHON=python3
+    PIP=pip3
+    echo "[+] Install for python3"
 fi
 
-PIP3="$(which pip3)"
-if [ $? -ne 0 ]; then
-    echo "[-] You need 'pip3' to install 'cemu' requirements"
-    exit 1
+# install build tools
+for req in git cmake ${PYTHON} ${PIP}
+do
+    dep="$(which $i)"
+    if [ $? -ne 0 ]; then
+        echo "[-] '${req}' is missing, installing..."
+        ${PKG} install ${req}
+    fi
+done
+
+set -e
+
+# installing enum module for python2
+if [ ${PIP} == "pip" ]; then
+    ${PIP} install enum34
+fi
+
+# install pyqt5
+if [ ${PKG}==brew ]; then
+    ${PKG} install pyqt5 pkg-config glib
+else
+    ${PKG} install ${PYTHON}-pyqt5 pkg-config libglib2.0-dev
 fi
 
 pushd .
 
-${PYTHON3} -c 'import keystone' 2>/dev/null
+# install keystone/capstone/unicorn
+${PYTHON} -c 'import keystone' 2>/dev/null
 if [ $? -ne 0 ]; then
     echo "[+] Installing keystone + bindings"
     cd /tmp
     git clone https://github.com/keystone-engine/keystone.git
     mkdir -p keystone/build && cd keystone/build
-    ../make-share.sh && make -j8
+    ../make-share.sh && make
     sudo make install
     cd ../bindings/python
-    sudo make install3
+    if [ ${PYTHON} == python ]; then
+        sudo make install
+    else
+        sudo make install3
+    fi
 fi
 
-${PYTHON3} -c 'import capstone' 2>/dev/null
+${PYTHON} -c 'import capstone' 2>/dev/null
 if [ $? -ne 0 ]; then
     echo "[+] Installing capstone + bindings"
     cd /tmp
@@ -43,10 +89,14 @@ if [ $? -ne 0 ]; then
     ./make.sh
     sudo ./make.sh install
     cd ./bindings/python
-    sudo make install3
+    if [ ${PYTHON} == python ]; then
+        sudo make install
+    else
+        sudo make install3
+    fi
 fi
 
-${PYTHON3} -c 'import unicorn' 2>/dev/null
+${PYTHON} -c 'import unicorn' 2>/dev/null
 if [ $? -ne 0 ]; then
     echo "[+] Installing unicorn + bindings"
     cd /tmp
@@ -55,9 +105,14 @@ if [ $? -ne 0 ]; then
     ./make.sh
     sudo ./make.sh install
     cd ./bindings/python
-    sudo make install3
+    if [ ${PYTHON} == python ]; then
+        sudo make install
+    else
+        sudo make install3
+    fi
 fi
 
 popd
 
-sudo ${PIP3} install -r ./requirements.txt
+# install pip missing packages
+sudo ${PIP} install pygments
