5395d864b9ea64ed4359ba92522ea11ee0289457
hugsy <crazy.hugsy@outlook.com>
2024-07-31 13:04:34 -0700

    started adding few more tests

---
+++ tests/test_arch.py
@@ -0,0 +1,22 @@
+def test_assemble_file():
+    pass
+
+
+def test_assemble():
+    pass
+
+
+def test_disassemble_file():
+    pass
+
+
+def test_disassemble():
+    pass
+
+
+def test_format_address():
+    pass
+
+
+def test_hexdump():
+    pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,12 @@
-import cemu.arch
-
-
 def test_get_metadata_from_stream():
-    from cemu.utils import get_metadata_from_stream
+    import cemu.arch
+    import cemu.utils
 
     raw = r"""
     ;;; @@@architecture x86_64
     ;;; @@@endianness little
     """
-    res = get_metadata_from_stream(raw)
+    res = cemu.utils.get_metadata_from_stream(raw)
     assert res and len(res) == 2
     assert isinstance(res[0], cemu.arch.Architecture)
     assert cemu.arch.is_x86_64(res[0])
@@ -21,27 +19,14 @@ def test_generate_random_string():
 
 
 def test_ishex():
-    pass
+    import cemu.utils
 
-
-def test_assemble_file():
-    pass
-
-
-def test_assemble():
-    pass
-
-
-def test_disassemble_file():
-    pass
-
-
-def test_disassemble():
-    pass
-
-
-def test_format_address():
-    pass
+    assert not cemu.utils.ishex("0Xasd")
+    assert cemu.utils.ishex("0123")
+    assert not cemu.utils.ishex("0123asd")
+    assert not cemu.utils.ishex("0x!!0123asd")
+    assert not cemu.utils.ishex("0123fff==")
+    assert cemu.utils.ishex("0123abcdef")
 
 
 def test_hexdump():
